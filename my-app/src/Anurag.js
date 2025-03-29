0356ec852faa6044e0ac7e510df5d4822e2ec357
RoshiniGella <roshinigella@gmail.com>
2025-03-29 14:13:28 +0530

    react

---
+++ my-app/src/Anurag.js
@@ -0,0 +1,8 @@
+const Anurag = () => {
+    return (
+        <div>
+            <h1>Hi Anurag</h1>
+        </div>
+    )
+}
+export default Anurag;
---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Anurag from './Anurag';
+function App() {
+  return (
+    <div className="App">
+      hiii
+      <Anurag/>
+      <Anurag/>
+      <Anurag/>
+      <Anurag/>
+      <Anurag/>
+    </div>
+  );
+}
+
+export default App;
