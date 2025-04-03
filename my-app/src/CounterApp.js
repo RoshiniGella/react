6a3b60a9f39d507a321abdb06393a135ff74e2fd
RoshiniGella <roshinigella@gmail.com>
2025-04-03 13:51:45 +0530

    useeffect

--- my-app/src/CounterApp.js
+++ my-app/src/CounterApp.js
@@ -1,34 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./CounterApp.css";
 const CounterApp = () => {
-    const [count, setCount] = useState(0); 
+    const [count, setCount] = useState(0);
+    const [counter, setCounter] = useState(0);
     const [backgroundColor, setBackgroundColor] = useState(true);
+    useEffect(() => {
+        console.log("useEffect is triggered")
+    }, [counter]);
 
+    const bgColor = backgroundColor ? "" : "bg2";
 
-    const bgColor = backgroundColor? "" : "bg2";
 
     const onIncrement = () => {
         setCount(count + 1)
     }
     const onDecrement = () => {
-        setCount(count - 1)
+        setCounter(counter - 1)
+        //setCount(count - 1)
     }
     let bg1 = "";
     let bg2 = "";
-    
+
     const reset = () => {
+        setCount(0);
         console.log(backgroundColor)
         setBackgroundColor(!backgroundColor);
         console.log(backgroundColor);
-        setCount(0);
     }
 
-    if (count < 0) {
-        console.log(count); 
-        setCount(0);
-    }
-    
-    
+    // if (count < 0) {
+    //     console.log(count); 
+    //     setCount(0);
+    // }
+
+
     if (count >= 5) {
         bg1 = "bg1";
     }
@@ -38,16 +43,18 @@ const CounterApp = () => {
     }
 
     return (
-
         <div className={bgColor}>
-        <h1 className={bg1}>
-            {count}
-        </h1>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
-        <button onClick={reset}> Reset</button>
+            <h1 className={bg1}>
+                {count}
+            </h1>
+            <h1>
+                {counter}
+            </h1>
+            <button onClick={onIncrement}>Increment</button>
+            <button onClick={onDecrement}>Decrement</button>
+            <button onClick={reset}> Reset</button>
         </div>
     )
-}   
+}
 
 export default CounterApp;
